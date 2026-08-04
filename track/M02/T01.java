0d1e3296a7a346a17bb09449a01425e4963b4c54
Sanjay Kumar B <sanjayboya2005@gmail.com>
2026-08-04 10:57:24 +0530

    Check1

--- track/M02/T01.java
+++ track/M02/T01.java
@@ -3,7 +3,7 @@ package track.M02;
 public class T01 {
     public static void main(String[] args) {
         System.out.println("Hello Kodnest");
-        int a=10;
+        int a = 11;
         System.out.println(a);
     }
 }
